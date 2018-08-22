0082b8d6d35eebbda4af94826b46534aa34fdf6b
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2018-08-22 19:22:43 +0530

    Changes date to ISO format

--- DMScraper/spiders/gov_news_spider.py
+++ DMScraper/spiders/gov_news_spider.py
@@ -40,6 +40,38 @@ class GovNewsSpider(scrapy.Spider):
                 if not (text_before == "Twitter"):
                     text += text_before
 
+        # print(date)
+        date_arr = date.replace(",", "").split()
+        date = date_arr[2] + "-"
+
+        if (date_arr[0] == "January"):
+            date += "01-"
+        elif (date_arr[0] == "February"):
+            date += "02-"
+        elif (date_arr[0] == "March"):
+            date += "03-"
+        elif (date_arr[0] == "April"):
+            date += "04-"
+        elif (date_arr[0] == "May"):
+            date += "05-"
+        elif (date_arr[0] == "June"):
+            date += "06-"
+        elif (date_arr[0] == "July"):
+            date += "07-"
+        elif (date_arr[0] == "August"):
+            date += "08-"
+        elif (date_arr[0] == "September"):
+            date += "09-"
+        elif (date_arr[0] == "October"):
+            date += "10-"
+        elif (date_arr[0] == "November"):
+            date += "11-"
+        elif (date_arr[0] == "December"):
+            date += "12-"
+
+        date += date_arr[1]
+        # print(date)
+
         if not (text == ""):
             output = {
                 'date': date,
@@ -51,7 +83,7 @@ class GovNewsSpider(scrapy.Spider):
             url = response.url
             file_name = url[40:44]
 
-            data_file = open("data_gov_news/" + file_name + ".json", "w+")
+            data_file = open("data_gov_news_new/" + file_name + ".json", "w+")
             data_file.write(json.dumps(output, indent=2, sort_keys=True))
             data_file.close()
 
